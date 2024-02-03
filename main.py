f6f076e7dda8523892bf644eb19dc987cc48f12b
ivan <ivan.khruchshev@gmail.com>
2024-02-03 17:41:38 +0500

    Переношу проект в гитхаб

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ work_chat_id = "-4158012020"
 if __name__ == '__main__':
     time_to_publish = "12:39"
     tz_to_publish = "UTC"
+    process(chat_id=chat_id)
     print("Запускаем расписание. Следующая публикация состоится в", time_to_publish, tz_to_publish)
     while True:
         schedule.every().day.at(time_to_publish, tz_to_publish).do(process, chat_id=chat_id)
