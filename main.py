035d9f7dc334555b67be749fad6c436b5445dec6
ivan <ivan.khruchshev@gmail.com>
2024-02-03 17:25:19 +0500

    Переношу проект в гитхаб

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from no1 import process
 import schedule
 
+
 chat_id = "-1002082261665"
 
 
 while True:
-    schedule.every().day.at("11:59", "UTC").do(process, chat_id=chat_id)
+    schedule.every().day.at("12:29", "UTC").do(process, chat_id=chat_id)
