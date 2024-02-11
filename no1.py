5bc41b127041528b57f63c1bdba31e9b67be71d0
ivan <ivan.khruchshev@gmail.com>
2024-02-11 19:24:03 +0500

    Реализована система отложенного постинга на 45 дней вперед. Теперь будущие посты записываются в таблицу. Эти данные можно будет потом использовать для работы с видео.

--- no1.py
+++ no1.py
@@ -202,9 +202,11 @@ def plan_post(post_date, chart_date, no1_list):
 
 def get_no1_planned_list(chart_date: date):
     ch_date = chart_date.isoformat()
+    print()
+    print(chart_date)
     print(ch_date)
     with conn.cursor() as curs:
-        curs.execute(planned_posts_for_date_sql, ch_date)
+        curs.execute(planned_posts_for_date_sql, chart_date)
         planned_list = curs.fetchall()
     return planned_list
 
