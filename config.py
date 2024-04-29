e12bbd61611df1d9e16e6c04b1c24e054a95ceb4
ivan <ivan.khruchshev@gmail.com>
2024-04-29 19:53:12 +0500

    Добавлено логирование обращений к боту. Сохранение идет в базу sqlite3

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ SUPER_BOT_TOKEN = os.getenv("MUSIC_SUPER_BOT_KEY", "")
 private_chat_id = os.getenv("PRIVATE_CHAT_ID", "")
 group_chat_id = os.getenv("GROUP_CHAT_ID", "")
 work_chat_id = os.getenv("WORK_CHAT_ID", "")
+admin_login = os.getenv("ADMIN_LOGIN", "")
 
 BASE_DIR = Path(__file__).resolve().parent
 
